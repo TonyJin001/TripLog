c5227c8af84ba0c9744c66bfad15e2c318ccea1a
vaasut <vaasu@vaasus-air.middlebury.edu>
2017-04-26 23:08:56 -0400

    files and stuff

---
+++ TripLog/TripTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TripTableViewCell.swift
+//  TripLog
+//
+//  Created by Vaasu on 4/24/17.
+//  Copyright © 2017 CS466. All rights reserved.
+//
+
+import UIKit
+
+class TripTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
