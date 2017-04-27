67ffe9e29b0ee4e3fbf379c1941475981743e12b
Lyra Ding <lyrading@lyras-mbp-2.middlebury.edu>
2017-04-26 23:24:41 -0400

    Added camera and upload functionalities in edit

--- TripLog/EditJournalViewController.swift
+++ TripLog/EditJournalViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 import CoreData
 
-class EditJournalViewController: UIViewController {
+class EditJournalViewController: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var tripNameTextField: UITextField!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var journalTextView: UITextView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var uploadButton: UIBarButtonItem!
     
     var journalEntryDetails:JournalEntry? = nil
     var callback : ((String,String,String,String)->Void)? = nil
@@ -51,6 +53,53 @@ class EditJournalViewController: UIViewController {
     }
 
 
+    @IBAction func importImage(_ sender: UIBarButtonItem) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        // Decide whether the user wants to take a photo or select it from the photo library
+        switch sender {
+        case cameraButton:
+            image.sourceType = UIImagePickerControllerSourceType.camera
+        case uploadButton:
+            image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        default:
+            fatalError("Source type for image picker unknown")
+        }
+        
+        image.allowsEditing = false
+        
+        self.present(image, animated: true) {
+            // After it's completed
+        }
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as?UIImage {
+            
+            let attachment = NSTextAttachment()
+            attachment.image = image
+            
+            // Scale the image
+            let oldWidth = attachment.image!.size.width
+            let scaleFactor = oldWidth/(journalTextView.frame.size.width-10)
+            attachment.image = UIImage(cgImage: attachment.image!.cgImage!, scale: scaleFactor, orientation:.up)
+            
+            //put your NSTextAttachment into and attributedString
+            let attString = NSAttributedString(attachment: attachment)
+            //add this attributed string to the current position.
+            journalTextView.textStorage.insert(attString, at: journalTextView.selectedRange.location)
+            
+        } else {
+            // Error
+            journalTextView.text = "Didn't get the image"
+        }
+        
+        
+        
+        self.dismiss(animated: true, completion: nil)
+    }
 
 
 
