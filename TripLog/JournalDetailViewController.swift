cd42d917bbc6cf8433d5489e8dc3c11eb535dbb6
Lyra Ding <lyrading@lyras-mbp-2.middlebury.edu>
2017-04-29 16:51:17 -0400

    Fixed textview scrolled to bottom bug. Cleaned up code

--- TripLog/JournalDetailViewController.swift
+++ TripLog/JournalDetailViewController.swift
@@ -50,6 +50,10 @@ class JournalDetailViewController: UIViewController {
         }
         
     }
+    
+    override func viewDidLayoutSubviews() {
+        self.textTextView.setContentOffset(.zero, animated: false)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -77,90 +81,53 @@ class JournalDetailViewController: UIViewController {
     
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        
-        switch(segue.identifier ?? ""){
-//        case "AddBook":
-//            guard let navController = segue.destination as? UINavigationController else{
-//                fatalError("Unexpected destination: \(segue.destination)")
-//            }
-//            guard let destination = navController.topViewController as? BookDetailViewController else{
-//                fatalError("Unexpected destination: \(segue.destination)")
-//            }
-//            destination.type = .new
-//            destination.callback = { (title, authorName, year) in
-//                self.books.add(title:title, authorName: authorName, year: year)
-//            }
-        case "EditJournal":
-            
-//            guard let navController = segue.destination as? UINavigationController else{
-//                fatalError("Unexpected destination: \(segue.destination)")
-//            }
-            
-            guard let destination = segue.destination as? EditJournalViewController else {
-                fatalError("Unexpected destination: \(segue.destination)")
-            }
-            
-//            guard let cell = sender as? BookListingCell else{
-//                fatalError("Unexpected sender: \(sender)")
-//            }
-//            
-//            guard let indexPath = tableView.indexPath(for: cell) else{
-//                fatalError("The selected cell can't be found")
-//            }
-            
-        
-            destination.journalEntryDetails = journalEntryDetails
-            destination.callback = { (text, date, location, tripName) in
-                self.journalEntries?.update(oldEntry: self.journalEntryDetails!, text:text, date:date, location:location, tripName:tripName)
-                self.newDate = date
-                self.newLocation = location
-                self.newText = text
-                self.newTripName = tripName
-                
-                let request = NSFetchRequest<NSFetchRequestResult>(entityName:"JournalEntry")
-//                request.fetchLimit = 1
-                
-                let datePredicate = NSPredicate(format: "date == %@", self.newDate)
-                let locationPredicate = NSPredicate(format: "location == %@", self.newLocation)
-                let textPredicate = NSPredicate(format: "text == %@", self.newText!)
-                let tripNamePredicate = NSPredicate(format: "trip.tripName == %@", self.newTripName)
-                let andPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [datePredicate,locationPredicate,textPredicate,tripNamePredicate])
-                
-                request.predicate = andPredicate
-                
-                do {
-                    let moc = self.journalEntries?.managedObjectContext
-                    let matches = try moc?.fetch(request)
-                    if matches?.count == 0 {
-                        fatalError("0 matches for the required journal entry!!!!")
-                    } else {
-                        self.journalEntryDetails = matches?[0] as? JournalEntry
-                        if let date = self.journalEntryDetails?.date {
-                            self.dateLabel.text = date
-                        }
-                        
-                        if let location = self.journalEntryDetails?.location {
-                            self.locationLabel.text = location
-                        }
-                        
-                        if let text = self.journalEntryDetails?.text {
-                            self.textTextView.attributedText = text as! NSAttributedString
-                        }
-                    }
-                } catch {
-                    fatalError("Cannot find journal entry")
-                }
 
-                
-                
-                
-            }
+        guard let destination = segue.destination as? EditJournalViewController else {
+            fatalError("Unexpected destination: \(segue.destination)")
+        }
         
+        destination.journalEntryDetails = journalEntryDetails
+        destination.callback = { (text, date, location, tripName) in
+            self.journalEntries?.update(oldEntry: self.journalEntryDetails!, text:text, date:date, location:location, tripName:tripName)
+            self.newDate = date
+            self.newLocation = location
+            self.newText = text
+            self.newTripName = tripName
             
-        default:
-            fatalError("Unexpeced segue identifier: \(segue.identifier)")
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName:"JournalEntry")
+            //                request.fetchLimit = 1
+            
+            let datePredicate = NSPredicate(format: "date == %@", self.newDate)
+            let locationPredicate = NSPredicate(format: "location == %@", self.newLocation)
+            let textPredicate = NSPredicate(format: "text == %@", self.newText!)
+            let tripNamePredicate = NSPredicate(format: "trip.tripName == %@", self.newTripName)
+            let andPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [datePredicate,locationPredicate,textPredicate,tripNamePredicate])
+            
+            request.predicate = andPredicate
+            
+            do {
+                let moc = self.journalEntries?.managedObjectContext
+                let matches = try moc?.fetch(request)
+                if matches?.count == 0 {
+                    fatalError("0 matches for the required journal entry!!!!")
+                } else {
+                    self.journalEntryDetails = matches?[0] as? JournalEntry
+                    if let date = self.journalEntryDetails?.date {
+                        self.dateLabel.text = date
+                    }
+                    
+                    if let location = self.journalEntryDetails?.location {
+                        self.locationLabel.text = location
+                    }
+                    
+                    if let text = self.journalEntryDetails?.text {
+                        self.textTextView.attributedText = text as! NSAttributedString
+                    }
+                }
+            } catch {
+                fatalError("Cannot find journal entry")
+            }
+            
         }
         
     }
